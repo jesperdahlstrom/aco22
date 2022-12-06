aed49d7bdc00065c3804ee405e39422f60880333
Jesper Dahlström <jesper.dahlstrom@forefront.se>
2022-12-06 19:24:14 +0100

    02 1 star done

--- tests/01.test.ts
+++ tests/01.test.ts
@@ -2,30 +2,27 @@ import { getSumArray, getHighetsCalorieCount, getTopThreeSum } from '../src/01/0
 import * as fs from 'fs';
 const file: string = fs.readFileSync('tests/testcases/01.txt', 'utf-8');
 
-describe('01', () => {
 
+test('getSumArray should return an array with specific values', () => {
 
-    test('getSumArray should return an array with specific values', () => {
+    const values: number[] = [
+        46224, 
+        52289, 
+        68154
+    ];
 
-        const values: number[] = [
-            46224, 
-            52289, 
-            68154
-        ];
-
-        expect(getSumArray(file)).toEqual(values);
-    })
-
-    test('getHighetsCalorieCount should return an integer', () => {
-        expect(getHighetsCalorieCount(file)).toBe(68154);
-    })
-
-    test('getTopThreeSum should return sum of the top values in the testcase file', () => {
-        expect(getTopThreeSum(file)).toBe(166667);
-    })
-
-
+    expect(getSumArray(file)).toEqual(values);
 });
+
+test('getHighetsCalorieCount should return an integer', () => {
+    expect(getHighetsCalorieCount(file)).toBe(68154);
+});
+
+test('getTopThreeSum should return sum of the top values in the testcase file', () => {
+    expect(getTopThreeSum(file)).toBe(166667);
+});
+
+
 
 
 
