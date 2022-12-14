6a91b0522410b567cb32825d334ea49b2c9b814b
Jesper Dahlström <jesper.dahlstrom@forefront.se>
2022-12-14 10:31:36 +0100

    second star 04

--- out/04/04.js
+++ out/04/04.js
@@ -23,9 +23,9 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculateOverlappingSections = exports.convertToCleaningArray = void 0;
+exports.calculateOverlappingSectionsTwoStar = exports.calculateOverlappingSections = exports.convertToCleaningArray = void 0;
 const fs = __importStar(require("fs"));
-const input = fs.readFileSync('../../../assets/04/04.txt', 'utf-8');
+const input = fs.readFileSync('../../assets/04/04.txt', 'utf-8');
 function convertToCleaningArray(input) {
     const sectionArray = input.toString().split("\n");
     return sectionArray.map(pair => pair.split(","));
@@ -50,8 +50,33 @@ function calculateOverlappingSections(cleaningArray) {
             count++;
         }
     });
-    console.log(count);
+    return count;
 }
 exports.calculateOverlappingSections = calculateOverlappingSections;
-calculateOverlappingSections(convertToCleaningArray(input));
+function calculateOverlappingSectionsTwoStar(cleaningArray) {
+    let count = 0;
+    cleaningArray.forEach((pair) => {
+        const firstPair = pair[0].split(',');
+        const secondPair = pair[1].split(',');
+        let firstPairSplit = firstPair[0].split('-');
+        let secondPairSplit = secondPair[0].split('-');
+        const firstPairF = parseInt(firstPairSplit[0]);
+        const firstPairS = parseInt(firstPairSplit[1]);
+        const secondPairF = parseInt(secondPairSplit[0]);
+        const secondPairS = parseInt(secondPairSplit[1]);
+        // 21-92
+        // 57-93
+        if (firstPairF >= secondPairF && firstPairF <= secondPairS || firstPairS >= secondPairF && firstPairS <= secondPairS) {
+            count++;
+            return;
+        }
+        if (secondPairF >= firstPairF && secondPairF <= firstPairS || secondPairS >= firstPairF && secondPairS <= firstPairS) {
+            count++;
+        }
+    });
+    return count;
+}
+exports.calculateOverlappingSectionsTwoStar = calculateOverlappingSectionsTwoStar;
+// calculateOverlappingSections(convertToCleaningArray(input))
+console.log(calculateOverlappingSectionsTwoStar(convertToCleaningArray(input)));
 //# sourceMappingURL=04.js.map
--- src/04/04.ts
+++ src/04/04.ts
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
-const input: string = fs.readFileSync('../../../assets/04/04.txt', 'utf-8');
+const input: string = fs.readFileSync('../../assets/04/04.txt', 'utf-8');
 
 
 export function convertToCleaningArray(input: string): string[][]{
@@ -9,13 +9,8 @@ export function convertToCleaningArray(input: string): string[][]{
 
 
 export function calculateOverlappingSections(cleaningArray: string[][]){
-
     let count: number = 0;
-
-    
-    
     cleaningArray.forEach((pair) => {
-        
         const firstPair = pair[0].split(',');
         const secondPair = pair[1].split(',');
         
@@ -35,23 +30,47 @@ export function calculateOverlappingSections(cleaningArray: string[][]){
         if(secondPairF >= firstPairF && secondPairS <= firstPairS && secondPairS <= firstPairS && secondPairS >= firstPairF){
             count++;
         }
-
-  
         
     });
+    return count
+}
+
+export function calculateOverlappingSectionsTwoStar(cleaningArray: string[][]){
+    let count: number = 0;
+    cleaningArray.forEach((pair) => {
+        const firstPair = pair[0].split(',');
+        const secondPair = pair[1].split(',');
+        
+        let firstPairSplit = firstPair[0].split('-')
+        let secondPairSplit = secondPair[0].split('-')
+
+        const firstPairF = parseInt(firstPairSplit[0]);
+        const firstPairS = parseInt(firstPairSplit[1]);
+        const secondPairF = parseInt(secondPairSplit[0]);
+        const secondPairS = parseInt(secondPairSplit[1]);        
 
 
+        // 21-92
+        // 57-93
 
-    console.log(count);
-    
+        if(firstPairF >= secondPairF && firstPairF <= secondPairS || firstPairS >= secondPairF && firstPairS <= secondPairS ){
+            count++;
+            return
+        }
 
-
+        if(secondPairF >= firstPairF && secondPairF <= firstPairS || secondPairS >= firstPairF && secondPairS <= firstPairS){
+            count++;
+        }
+        
+    });
+    return count
 }
 
 
 
 
-calculateOverlappingSections(convertToCleaningArray(input))
+// calculateOverlappingSections(convertToCleaningArray(input))
+console.log(calculateOverlappingSectionsTwoStar(convertToCleaningArray(input)));
 
 
 
